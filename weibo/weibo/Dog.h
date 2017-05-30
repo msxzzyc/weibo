46d333731d59e6515b0d64c3c1f8fe4be19c44a0
zyc <10168363742qq.com>
2017-05-30 17:52:00 +0800

    创建Dog类

---
+++ weibo/weibo/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weibo
+//
+//  Created by wpzyc on 2017/5/30.
+//  Copyright © 2017年 wpzyc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weibo/weibo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weibo
+//
+//  Created by wpzyc on 2017/5/30.
+//  Copyright © 2017年 wpzyc. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
